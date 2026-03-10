f35a2566243c1b665151e9e76dba58ecb5bbc3be
Faisal Liaquat <mfaisal1801@protonmail.com>
2026-03-11 00:00:23 +0500

    fix(build): add --yes flag to migrate, add blank migration to resolve dev prompt

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260308_175334 from './20260308_175334';
 import * as migration_20260310_142528 from './20260310_142528';
+import * as migration_20260310_185944 from './20260310_185944';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260310_142528.up,
     down: migration_20260310_142528.down,
-    name: '20260310_142528'
+    name: '20260310_142528',
+  },
+  {
+    up: migration_20260310_185944.up,
+    down: migration_20260310_185944.down,
+    name: '20260310_185944'
   },
 ];
