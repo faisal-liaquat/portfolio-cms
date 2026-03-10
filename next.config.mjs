3a69b24a92411ccccdf76f26dbdf9b05d4c2b188
Faisal Liaquat <mfaisal1801@protonmail.com>
2026-03-10 18:53:14 +0500

    chore(config): add SimpleIcons CDN to allowed image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'cdn.simpleicons.org' },
     ],
   },
 }
