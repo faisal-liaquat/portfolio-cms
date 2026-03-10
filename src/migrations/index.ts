79c0790e1120e25e95497c4f6c80a1fe7230d896
Faisal Liaquat <mfaisal1801@protonmail.com>
2026-03-10 19:36:07 +0500

    chore(db): migration for site-settings nowBarChips and lastCommit fields

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260308_175334 from './20260308_175334';
+import * as migration_20260310_142528 from './20260310_142528';
 
 export const migrations = [
   {
     up: migration_20260308_175334.up,
     down: migration_20260308_175334.down,
-    name: '20260308_175334'
+    name: '20260308_175334',
+  },
+  {
+    up: migration_20260310_142528.up,
+    down: migration_20260310_142528.down,
+    name: '20260310_142528'
   },
 ];
